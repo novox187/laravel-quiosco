9412acca12006942fffe11c65f975a007e82e97a
Harrison Ochoa <novox187@gmail.com>
2024-02-28 01:58:16 +0300

    Validacion y Guardar Categorias en DB

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoriaRequest;
 use App\Http\Resources\CategoriaCollection;
 use App\Models\Categoria;
 use Illuminate\Http\Request;
@@ -11,5 +12,21 @@ class CategoriaController extends Controller
     public function index(){
         
         return new CategoriaCollection(Categoria::all());
+    }
+    public function store(CategoriaRequest $request){
+
+        $datos = $request->validated();
+
+        $iconoName = 'icono_'.time().'.'.$request->icono->extension();  
+        $nombreLimpio = pathinfo($iconoName, PATHINFO_FILENAME);
+   
+        $request->icono->move(public_path('img'), $iconoName);
+
+        $categorias = new Categoria;
+        $categorias->nombre =$request->nombre;
+        $categorias->icono = $nombreLimpio;
+        $categorias->save();
+
+        return response()->json(['success'=>'Categoria guardada correctamente.']);
     }
 }
---
+++ app/Http/Requests/CategoriaRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoriaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nombre' => ['required', 'unique:categorias,nombre'],
+            'icono' => ['required', 'unique:categorias,icono']
+        ];
+    }
+
+    public function messages(){
+        return[
+            'nombre' => 'El Nombre de la categoria es requerido',
+            'icono' => 'El Icono de la categoria es requerido'
+        ];
+    }
+}
