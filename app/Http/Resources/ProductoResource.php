d0eddfc2a934c5a2c1f53e1f6a2b2818db341bcc
Harrison Ochoa <novox187@gmail.com>
2024-05-31 12:06:58 +0300

    add created_at

--- app/Http/Resources/ProductoResource.php
+++ app/Http/Resources/ProductoResource.php
@@ -25,6 +25,7 @@ class ProductoResource extends JsonResource
             'promo_id' => $this->promo_id,
             'rating' => $this->rating,
             'categoria_id' => $this->categoria_id,
+            'created_at' => $this->created_at,
             'promocion' => $this->whenLoaded('promocion', function () {
                 return [
                     'id' => $this->promocion->id,
