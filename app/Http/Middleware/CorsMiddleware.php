a37f3857f55c17431a5a976e73410437fb6f2001
Harrison Ochoa <novox187@gmail.com>
2024-06-04 13:22:21 +0300

    ajustes de Cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -10,7 +10,7 @@ class CorsMiddleware
     {
         $response = $next($request);
     
-        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Origin', 'https://www.famasteak.com');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     
--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://famasteak.com',
+        'https://react-quiosco-desarrollo.up.railway.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
