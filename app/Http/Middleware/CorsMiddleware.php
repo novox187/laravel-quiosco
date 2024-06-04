e4da13152af571c284a6037ca57b7064e9d6709a
Harrison Ochoa <novox187@gmail.com>
2024-06-04 14:15:55 +0300

    nueva solucion de cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -12,10 +12,8 @@ class CorsMiddleware
 
         $response->headers->set('Access-Control-Allow-Origin', 'https://www.famasteak.com');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->headers->set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
         $response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate');
-        $response->headers->set('Pragma', 'no-cache');
-        $response->headers->set('Expires', '0');
 
         return $response;
     }
