ed3fe0b628c9a49a5873613bb79a4331d2648dcc
Harrison Ochoa <novox187@gmail.com>
2024-06-04 13:53:37 +0300

    Ajustes de cache 6

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -9,11 +9,14 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-    
+
         $response->headers->set('Access-Control-Allow-Origin', 'https://www.famasteak.com');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    
+        $response->headers->set('Cache-Control', 'no-cache, no-store, must-revalidate');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', '0');
+
         return $response;
     }
 }
