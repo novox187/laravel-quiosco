59a3e4b541dd42469b68eaa4851b11bff5ce69c2
Harrison Ochoa <novox187@gmail.com>
2024-06-05 01:18:56 +0300

    Correccion de cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://www.famasteak.com'],
 
     'allowed_origins_patterns' => [],
 
--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['corsMiddleware', 'auth:sanctum'])->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [UserController::class, 'usuarioEnSession']);
 
     Route::post('/logout', [AuthController::class, 'logout']);
