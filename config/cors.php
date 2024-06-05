73c528d5b640c09577a3b9fee4d19704e5217626
Harrison Ochoa <novox187@gmail.com>
2024-06-05 13:38:12 +0300

    permitiendo todos los allowed_origins_patterns

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['*'], // Permitir la exposición de todos los encabezados de respuesta
     'max_age' => 0,
