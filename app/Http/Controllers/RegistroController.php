76384fcd4508f2dae0e1197b5f7d278024aea9b3
Harrison Ochoa <novox187@gmail.com>
2024-07-04 17:17:03 +0300

    registro pedidos y promociones informe

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -26,6 +26,7 @@ class RegistroController extends Controller
             ->first();
 
         if ($registros->accion === 'editar') {
+
             $registrosd2 = Registro::where('producto_id', $registros->producto_id)
                 ->orderBy('id', 'desc')
                 ->where('id', '<', $registros->id)
@@ -40,28 +41,41 @@ class RegistroController extends Controller
                 'datos' => new RegistroResource($registros),
                 'cambios' => $this->encontrarDiferencias($viejo, $nuevo)
             ];
-        } else if($registros->accion === 'crear' || $registros->accion === 'eliminar' && $registros->producto !== null){
+        } else if ($registros->accion === 'crear' || $registros->accion === 'eliminar' && $registros->producto !== null) {
             return [
                 'datos' => new RegistroResource($registros),
             ];
-        }else if($registros->accion === 'cambiar_estado'){
+        } else if ($registros->accion === 'cambiar_estado') {
             return [
                 'datos' => new RegistroResource($registros),
                 'estado' => json_decode($registros->detalle)
             ];
-        }else if ($registros->accion === 'cobro') {
+        } else if ($registros->accion === 'cobro' || $registros->accion === 'entrega' || $registros->accion === 'preparacion' && $registros->pedido !== null) {
             return [
                 'datos' => new RegistroResource($registros),
             ];
-        }else if ($registros->accion === 'entrega') {
+        } else if ($registros->accion === 'crear' || $registros->accion === 'eliminar' && $registros->categoria !== null) {
             return [
                 'datos' => new RegistroResource($registros),
             ];
-        }else if ($registros->accion === 'preparacion') {
+        } else if ($registros->accion === 'editar' && $registros->categoria !== null) {
+            $registrosd2 = Registro::where('categoria_id', $registros->categoria_id)
+                ->orderBy('id', 'desc')
+                ->where('id', '<', $registros->id)
+                ->first();
+
+            $nuevo = json_decode($registros->detalle, true);
+            $viejo = json_decode($registrosd2->detalle, true);
+
+            return [
+                'datos' => new RegistroResource($registros),
+                'cambios' => $this->encontrarDiferencias($viejo, $nuevo)
+            ];
+        } else if ($registros->accion === 'crear' || $registros->accion === 'eliminar'  && $registros->promocion !== null) {
             return [
                 'datos' => new RegistroResource($registros),
             ];
-        }else{
+        } else {
             return [
                 'datos' => new RegistroResource($registros),
                 'cambios' => [
@@ -79,6 +93,7 @@ class RegistroController extends Controller
             if (array_key_exists($key, $d2) && $d2[$key] !== $value) {
                 if ($key === 'descripcion') {
                     $cambios[] = "Modificó la descripción";
+                } else if ($key === 'updated_at' || $key === 'created_at') {
                 } else {
                     $cambios[] = "Cambió el $key de " . (is_string($value) ? $value : json_encode($value)) . " a " . (is_string($d2[$key]) ? $d2[$key] : json_encode($d2[$key]));
                 }
@@ -97,5 +112,25 @@ class RegistroController extends Controller
 
 
         return  $cambios;
+    }
+
+    private function formatearFecha($fechaISO)
+    {
+        $fecha = Carbon::parse($fechaISO);
+        $dia = $fecha->day;
+        $mes = $fecha->month - 1; // Los meses en Carbon son indexados desde 1 (enero) a 12 (diciembre)
+        $anio = $fecha->year;
+        $horas = $fecha->hour;
+        $minutos = $fecha->minute;
+
+        $mesesEnEspanol = [
+            "enero", "febrero", "marzo", "abril", "mayo", "junio",
+            "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+        ];
+
+        // Formatea los minutos para que siempre tengan dos dígitos
+        $minutosFormateados = str_pad($minutos, 2, '0', STR_PAD_LEFT);
+
+        return "{$dia} de {$mesesEnEspanol[$mes]} de {$anio} a las {$horas}:{$minutosFormateados}";
     }
 }
