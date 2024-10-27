c5b59cee5c3921ea2df69ac1460b5c7bab630327
Harrison Ochoa <novox187@gmail.com>
2024-10-27 10:41:54 +0300

    creacion de tabla,modelo,controlador de cierres_caja

---
+++ app/Models/Cierres_caja.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cierres_caja extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2024_10_27_073632_create_cierres_cajas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('cierres_caja', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_caja');
+            $table->foreign('id_caja')->references('id')->on('cajas');
+            $table->unsignedBigInteger('id_apertura');
+            $table->foreign('id_apertura')->references('id')->on('aperturas_caja');
+            $table->decimal('monto_final', 10, 2);
+            $table->decimal('total_ventas', 10, 2);
+            $table->decimal('discrepancia', 10, 2)->nullable();
+            $table->unsignedBigInteger('usuario_cierre');
+            $table->unsignedBigInteger('usuario_modificacion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('cierres_caja');
+    }
+};
