3737934a34a86f2b31e91ebbc0e4752d57b0e242
Harrison Ochoa <novox187@gmail.com>
2024-10-27 11:10:26 +0300

    creacion de modal, tabla, controlador de Transacciones

---
+++ app/Http/Controllers/TransaccionesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transacciones;
+use Illuminate\Http\Request;
+
+class TransaccionesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Transacciones $transacciones)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Transacciones $transacciones)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Transacciones $transacciones)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Transacciones $transacciones)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_10_27_074240_create_transacciones_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('transacciones', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_caja');
+            $table->foreign('id_caja')->references('id')->on('cajas');
+            $table->unsignedBigInteger('id_apertura');
+            $table->foreign('id_apertura')->references('id')->on('aperturas_caja');
+            $table->unsignedBigInteger('id_pedido');
+            $table->foreign('id_pedido')->references('id')->on('pedidos');
+            $table->unsignedBigInteger('usuario_modificacion')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('transacciones');
+    }
+};
